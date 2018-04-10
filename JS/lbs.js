52b7e0d324a299a400c9c879795e8eda451c5ed8
Ungarala Suresh <ungarala@bldm2891-mac.corp.bluejeans.com>
2018-04-10 15:24:14 +0530

    changed reverse increasing logic

--- JS/lbs.js
+++ JS/lbs.js
@@ -1,6 +1,6 @@
-var sure=[10, 22, 9, 21, 33, 50, 41, 60],lis=[],lds=[],large=0;
+var sure=[10, 22, 9, 21, 33, 50, 41, 60],lis=[],rlis=[],large=0,rev=0,lbs=0,index=0;
 for(var i=0;i<sure.length;i++){
-    lis.push(1),lds.push(1);
+    lis.push(1),rlis.push(1);
 }
 for(var i=1;i<sure.length;i++){
     for(var j=0;j<i;j++){
@@ -10,9 +10,29 @@ for(var i=1;i<sure.length;i++){
     }
 }
 
-for(var i=0;i<lis.length;i++){
-    if(lis[i] > large){
-        large=lis[i];
+for(var i=sure.length-2;i>=0;i--){
+    for(var j=sure.length-1;j>i;j--){
+        if(sure[i] > sure[j] && rlis[i] < rlis[j]+1){
+            rlis[i] = rlis[j]+1;
+        }
     }
 }
-console.log(large);
+// for(var i=0;i<lis.length;i++){
+//     if(lis[i] > large){
+//         large=lis[i];
+//     }
+// }
+// for(var i=0;i<rlis.length;i++){
+//     if(rlis[i] > rev){
+//         rev=rlis[i];
+//     }
+// }
+console.log(lis,rlis);
+//console.log(large,rev);
+for(var i=0;i<sure.length;i++){
+    if(lis[i]+rlis[i]-1 > lbs){
+        index=i;
+        lbs=lis[i]+rlis[i]-1;
+    }
+}
+console.log(lbs,index);
