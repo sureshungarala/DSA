ecd2fc667d9b0ae8ad456a5719eeca285608b9ff
sureshUngarala <iamuvvsuresh@gmail.com>
2018-04-15 12:39:29 +0530

    updated with path

--- JS/TSP.js
+++ JS/TSP.js
@@ -7,25 +7,29 @@ var sure = [[0, 10, 15, 20],
 
 var minPath = Number.MAX_SAFE_INTEGER, marr = [0, 1, 2, 3];
 
-function find(arr, k, m) {
+function find(arr, k, m, tmp) {
+    tmp.push(k);
     arr = arr.filter(function (num) {
         return num != k;
     });
     var min = Number.MAX_SAFE_INTEGER;
     if (arr.length > 1) {
         for (var i = 0; i < arr.length; i++) {
-            var temp = sure[k][arr[i]] + find(arr, arr[i], m);
+            var t = JSON.parse(JSON.stringify(tmp));
+            var temp = sure[k][arr[i]] + find(arr, arr[i], m, t);
             if (temp < min) {
                 min = temp;
             }
         }
     } else {
+        tmp.push(arr[0]);
+        console.log(tmp);
         return sure[k][arr[0]] + sure[arr[0]][m];
     }
     return min;
 }
 for (var i = 0; i < marr.length; i++) {
-    var temp = find(marr, marr[i], marr[i]);
+    var temp = find(marr, marr[i], marr[i], []);
     if (minPath > temp) {
         minPath = temp;
     }
